b412e80566501140d9a7422fdc497d5b3d463ff7
Carsen <carse.astro@gmail.com>
2025-03-09 14:42:07 -0700

    Implemented simple api endpoint example, connected to frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,28 @@
+'use client';
+
+import { useEffect, useState } from "react";
 import Image from "next/image";
 
+interface ApiResponse {
+    message: string;
+}
+
 export default function Home() {
+    const [data, setData] = useState<ApiResponse | null>(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch('/api/test');
+            const result = await response.json();
+            setData(result);
+        }
+
+        fetchData();
+    }, []);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1>API RESPONSE: {data?.message}</h1>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
