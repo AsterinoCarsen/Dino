960d65e55deda9dd139388d21e9708f29cdcf9fc
Carsen Asterino <carse.astro@gmail.com>
2026-05-09 14:46:52 -0700

    Created dockerfile for deployment of API and added .env.example to root

--- apps/api/Program.cs
+++ apps/api/Program.cs
@@ -76,4 +76,10 @@ app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ClimbingLogContext>();
+    db.Database.Migrate();
+}
+
 app.Run();
