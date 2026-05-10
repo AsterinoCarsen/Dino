2e1f56fb00b55b4d61172292dd6c8030cf05e613
Carsen Asterino <carse.astro@gmail.com>
2026-05-09 19:39:40 -0700

    Changed CORS policy for production.

--- apps/api/Program.cs
+++ apps/api/Program.cs
@@ -16,7 +16,11 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("DevPolicy", policy =>
     {
-        policy.WithOrigins("http://localhost:3000")
+        policy.WithOrigins(
+            "http://localhost:3000",
+            "https://asterino.dev",
+            "https://www.asterino.dev"
+        )
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
